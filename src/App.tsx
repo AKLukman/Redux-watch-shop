88d94fc4883aa6971d9b733489aa20bdc3b83b30
AKLukman <abulkashemlukman@gmail.com>
2023-09-19 22:26:08 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Toaster } from './components/ui/Toaster';
 import MainLayout from './layouts/MainLayout';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { useAppDispatch } from './redux/hook';
+import { setLoading, setUser } from './redux/features/user/userSlice';
+import { useEffect } from 'react';
 
 function App() {
+  const auth = getAuth();
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.email));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  });
+
   return (
     <div>
       <Toaster />
